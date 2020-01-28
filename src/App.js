ae19a4ad99c232c63db4db564deab26ee0aca61c
bebyx <artem.bebyk@gmail.com>
2020-01-28 22:50:16 +0200

    Add auto-synchronized input for miles

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Check miles</h1>
+        <h1>Check speed</h1>
         <Calculator />
       </header>
     </div>
@@ -14,7 +14,7 @@ function App() {
 
 function RoadCheck(props) {
 	if (!props.km) {
-		return <p>Please type your speed in km.</p>;
+		return <p>Please type your speed in either unit.</p>;
 	} else if (props.km > 130) {
 		return <p>Not allowed</p>;
 	} else if (props.km > 50) {
@@ -24,24 +24,87 @@ function RoadCheck(props) {
 	}
 }
 
-class Calculator extends React.Component {
+const unitNames = {
+	km: 'km',
+	mi: 'mi'
+}
+
+function toKilometers(mi) {
+	return mi * 1.609344;
+}
+
+function toMiles(km) {
+	return km * 0.6213712;
+}
+
+function tryConvert(speed, convert) {
+	const input = parseFloat(speed);
+	if (Number.isNaN(input)) {
+		return '';
+	}
+	const output = convert(input);
+	const rounded = Math.round(output * 10) / 10;
+  	return rounded.toString();
+}
+
+
+class SpeedInput extends React.Component {
 	constructor(props) {
 		super(props);
 		this.handleChange = this.handleChange.bind(this);
-		this.state = {km: ''};
 	}
 
 	handleChange(event) {
-		this.setState({km: event.target.value});
+		this.props.onSpeedChange(event.target.value);
 	}
 
 	render() {
-		const km = this.state.km;
+		const speed = this.props.speed;
+		const unit = this.props.unit;
 		return (
 			<div>
-				<input value={km} onChange={this.handleChange} />
-				
-				<RoadCheck km={parseInt(km)} />
+				<label>{unitNames[unit]}: </label>
+				<input value={speed} onChange={this.handleChange} />
+			</div>
+			);
+	}
+}
+
+
+class Calculator extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleKilometersChange = this.handleKilometersChange.bind(this);
+		this.handleMilesChange = this.handleMilesChange.bind(this);
+		this.state = {speed: '', unit: 'km'};
+	}
+
+	handleKilometersChange(speed) {
+		this.setState({unit: 'km', speed});
+	}
+
+	handleMilesChange(speed) {
+		this.setState({unit: 'mi', speed});
+	}
+
+	render() {
+		const unit = this.state.unit;
+		const speed = this.state.speed;
+		const kilometers = unit === 'mi' ? tryConvert(speed, toKilometers) : speed;
+		const miles = unit === 'km' ? tryConvert(speed, toMiles) : speed;
+		return (
+			<div>
+				<SpeedInput				
+				 unit="km"
+				 speed={kilometers}
+				 onSpeedChange={this.handleKilometersChange} />
+				<br/>
+				<SpeedInput 
+				 unit="mi"
+				 speed={miles}
+				 onSpeedChange={this.handleMilesChange} />
+
+				<RoadCheck km={parseFloat(kilometers)} />
 			</div>
 			);
 	}
