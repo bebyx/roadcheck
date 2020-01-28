1e647de706eab465ab3c35f55ebacab3a023d214
bebyx <artem.bebyk@gmail.com>
2020-01-28 18:59:54 +0200

    Add function that checks speed on the road

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,9 +6,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Check miles</h1>
+        <RoadCheck km={30} />
       </header>
     </div>
   );
 }
+
+function RoadCheck(props) {
+	if (props.km > 130) {
+		return <p>Not allowed</p>;
+	} else if (props.km > 50) {
+		return <p>Allowed on Autobahns. Not allowed in cities.</p>;
+	} else {
+		return <p>Allowed.</p>;
+	}
+}
+
+
 
 export default App;
