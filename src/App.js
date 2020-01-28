60e38a7f329069e1a541130c7e90c5410825ef6a
bebyx <artem.bebyk@gmail.com>
2020-01-28 20:04:12 +0200

    Add input for km

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Check miles</h1>
-        <RoadCheck km={30} />
+        <Calculator />
       </header>
     </div>
   );
 }
 
 function RoadCheck(props) {
-	if (props.km > 130) {
+	if (!props.km) {
+		return <p>Please type your speed in km.</p>;
+	} else if (props.km > 130) {
 		return <p>Not allowed</p>;
 	} else if (props.km > 50) {
 		return <p>Allowed on Autobahns. Not allowed in cities.</p>;
@@ -22,6 +24,29 @@ function RoadCheck(props) {
 	}
 }
 
+class Calculator extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.state = {km: ''};
+	}
+
+	handleChange(event) {
+		this.setState({km: event.target.value});
+	}
+
+	render() {
+		const km = this.state.km;
+		return (
+			<div>
+				<input value={km} onChange={this.handleChange} />
+				
+				<RoadCheck km={parseInt(km)} />
+			</div>
+			);
+	}
+
+}
 
 
 export default App;
